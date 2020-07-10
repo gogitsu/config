8e94939f921b9f00469bd0970d465f1f4c9a1daf
joshuagame <joshuagame@gmail.com>
2020-07-10 12:24:48 +0200

    LICENSE and ATTRIBUTIONS files

--- config.go
+++ config.go
@@ -1,1 +1,12 @@
+// Copyright 2020 Luca Stasio. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+//
+// The gogitsu/config is heavily based on cleanenv by
+// (c) 2019 Ilya Kaznacheev
+// https://github.com/ilyakaznacheev/cleanenv
+// cleanenv copyright notice and permission notice can
+// be found in the ATTRIBUTIONS file.
+
+// Package config implements configuration components of gogitsu lib.
 package config
