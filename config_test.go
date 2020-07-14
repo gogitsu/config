85a302fdda8c38d856ce0e992d6a436714caefea
Luca Stasio <joshuagame@gmail.com>
2020-07-14 17:07:34 +0200

    config load from file

--- config_test.go
+++ config_test.go
@@ -8,8 +8,13 @@ import (
 
 var jsonData = `{"host": "localhost","port": 8080}`
 
-const ExpectedCfgHost = "127.0.0.1"
-const ExpectedCfgPort = 8080
+const (
+	ExpectedCfgPort        = 8080
+	ExpectedCfgHost        = "localhost"
+	ExpectedServiceGroup   = "test"
+	ExpectedServiceName    = "NAME-FROM-ENV"
+	ExpectedServiceVersion = "0.0.1"
+)
 
 type JSONConfig struct {
 	Host string `json:"host"`
@@ -26,23 +31,40 @@ type Cfg struct {
 
 func TestConfigurator(t *testing.T) {
 	c := NewConfigurator().WithFormat("json")
-	t.Logf("c.paths: %+v, c.fileName: '%s'", c.paths, c.FileName())
 	cfg := JSONConfig{}
 	err := c.Parser().Parse(strings.NewReader(jsonData), &cfg)
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
-	t.Logf("CONFIGURATION: %+v\n", cfg)
+
+	if cfg.Host != ExpectedCfgHost {
+		t.Fatalf("cfg.Host expected '%s' is '%s'", ExpectedCfgHost, cfg.Host)
+	}
+
+	if cfg.Port != ExpectedCfgPort {
+		t.Fatalf("cfg.Port expected '%d' is '%d'", ExpectedCfgPort, cfg.Port)
+	}
 }
 
 func TestConfiguratorFromFile(t *testing.T) {
 	os.Setenv("ENV", "test")
-	// os.Setenv("SVC_NAME", "service-name-from-env")
+	os.Setenv("SVC_NAME", ExpectedServiceName)
 	var config *Cfg = &Cfg{}
 	c := NewConfiguratorFor("yaml")
 	err := c.Load(config)
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
-	t.Logf("CONFIGURATION: %+v\n", config)
+
+	if config.Service.Group != ExpectedServiceGroup {
+		t.Fatalf("Service.Group expected '%s' is '%s'", ExpectedServiceGroup, config.Service.Group)
+	}
+
+	if config.Service.Name != ExpectedServiceName {
+		t.Fatalf("Service.Name expected '%s' is '%s'", ExpectedServiceName, config.Service.Name)
+	}
+
+	if config.Service.Version != ExpectedServiceVersion {
+		t.Fatalf("Service.Version expected '%s' is '%s'", ExpectedServiceVersion, config.Service.Version)
+	}
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/BurntSushi/toml v0.3.1
+	github.com/go-mach/gm-config v0.0.0-20190711141405-3918a3917e5e
 	github.com/joho/godotenv v1.3.0
 	gopkg.in/yaml.v2 v2.3.0
 )
